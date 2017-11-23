b4bbbc82688b5dd77bb9a97789e10a196db4fcfa
SpacemanSpliff99 <33433017+SpacemanSpliff99@users.noreply.github.com>
2017-11-23 17:47:12 -0400

    Update and rename amsterdamcoind.cpp to puffscoind.cpp

--- src/puffscoind.cpp
+++ src/puffscoind.cpp
@@ -2,6 +2,7 @@
 // Copyright (c) 2009-2014 The Bitcoin developers
 // Copyright (c) 2014-2015 The Dash developers
 // Copyright (c) 2015-2017 The AmsterdamCoin developers
+// Copyright (c) 2017 The PUFFScoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -24,9 +25,11 @@
  *
  * \section intro_sec Introduction
  *
- * This is the developer documentation of the reference client for an experimental new digital currency called AmsterdamCoin (http://www.amsterdamcoin.com),
- * which enables instant payments to anyone, anywhere in the world. AmsterdamCoin uses peer-to-peer technology to operate
- * with no central authority: managing transactions and issuing money are carried out collectively by the network.
+ * This is the developer documentation of the reference client for an experimental new digital currency called PUFFScoin (http://www.puffscoin.ca),
+ * which enables instant payments to anyone, anywhere in the world. PUFFScoin uses peer-to-peer technology to managing transactions with
+ * trustless ease, and features masternode services for transaction obfuscation and zero-confirm instant transactions.
+ * 
+ * PUFFScoin is the proprietary token for The Leafy Cauldron Apothecary.
  *
  * The software is a community-driven open source project, released under the MIT license.
  *
@@ -64,18 +67,18 @@ bool AppInit(int argc, char* argv[])
     //
     // Parameters
     //
-    // If Qt is used, parameters/amsterdamcoin.conf are parsed in qt/amsterdamcoin.cpp's main()
+    // If Qt is used, parameters/puffscoin.conf are parsed in qt/puffscoin.cpp's main()
     ParseParameters(argc, argv);
 
     // Process help and version before taking care about datadir
     if (mapArgs.count("-?") || mapArgs.count("-help") || mapArgs.count("-version")) {
-        std::string strUsage = _("AmsterdamCoin Core Daemon") + " " + _("version") + " " + FormatFullVersion() + "\n";
+        std::string strUsage = _("PUFFScoin Core Daemon") + " " + _("version") + " " + FormatFullVersion() + "\n";
 
         if (mapArgs.count("-version")) {
             strUsage += LicenseInfo();
         } else {
             strUsage += "\n" + _("Usage:") + "\n" +
-                        "  amsterdamcoind [options]                     " + _("Start AmsterdamCoin Core Daemon") + "\n";
+                        "  puffscoind [options]                     " + _("Start PUFFScoin Core Daemon") + "\n";
 
             strUsage += "\n" + HelpMessage(HMM_BITCOIND);
         }
@@ -111,17 +114,17 @@ bool AppInit(int argc, char* argv[])
         // Command-line RPC
         bool fCommandLine = false;
         for (int i = 1; i < argc; i++)
-            if (!IsSwitchChar(argv[i][0]) && !boost::algorithm::istarts_with(argv[i], "amsterdamcoin:"))
+            if (!IsSwitchChar(argv[i][0]) && !boost::algorithm::istarts_with(argv[i], "puffscoin:"))
                 fCommandLine = true;
 
         if (fCommandLine) {
-            fprintf(stderr, "Error: There is no RPC client functionality in amsterdamcoind anymore. Use the amsterdamcoin-cli utility instead.\n");
+c            fprintf(stderr, "Error: There is no RPC client functionality in puffsoind anymore. Use the puffscoin-cli utility instead.\n");
             exit(1);
         }
 #ifndef WIN32
         fDaemon = GetBoolArg("-daemon", false);
         if (fDaemon) {
-            fprintf(stdout, "AmsterdamCoin server starting\n");
+            fprintf(stdout, "PUFFScoin server starting\n");
 
             // Daemonize
             pid_t pid = fork();
@@ -174,7 +177,7 @@ int main(int argc, char* argv[])
 {
     SetupEnvironment();
 
-    // Connect amsterdamcoind signal handlers
+    // Connect puffscoind signal handlers
     noui_connect();
 
     return (AppInit(argc, argv) ? 0 : 1);
