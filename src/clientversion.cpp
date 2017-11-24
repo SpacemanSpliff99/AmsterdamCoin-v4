e58280120a9858a7c2a35aa5c79629b9036bd01c
SpacemanSpliff99 <33433017+SpacemanSpliff99@users.noreply.github.com>
2017-11-24 11:41:40 -0400

    Update clientversion.cpp

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -10,10 +10,10 @@
 
 /**
  * Name of client reported in the 'version' message. Report the same name
- * for both amsterdamcoind and amsterdamcoin-qt, to make it harder for attackers to
+ * for both puffscoind and puffscoin-qt, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("AmsterdamCoin Core");
+const std::string CLIENT_NAME("PuffsCoin Core");
 
 /**
  * Client version number
