cd0e4905320e94b8a6f541c77634e76ca8da8432
SpacemanSpliff99 <33433017+SpacemanSpliff99@users.noreply.github.com>
2017-11-24 21:53:27 -0400

    Update rpcserver.h

--- src/rpcserver.h
+++ src/rpcserver.h
@@ -1,5 +1,6 @@
 // Copyright (c) 2010 Satoshi Nakamoto
 // Copyright (c) 2009-2014 The Bitcoin developers
+// Copyright (c) 2017 The PuffsCoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -95,7 +96,7 @@ public:
 };
 
 /**
- * AmsterdamCoin RPC command dispatcher.
+ * PuffsCoin RPC command dispatcher.
  */
 class CRPCTable
 {
