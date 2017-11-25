a6723b7e6bc016ce2dc0f81f6d65615dbaca3d94
SpacemanSpliff99 <33433017+SpacemanSpliff99@users.noreply.github.com>
2017-11-24 21:39:16 -0400

    Update puffscoin-cli.cpp

--- src/puffscoin-cli.cpp
+++ src/puffscoin-cli.cpp
@@ -2,7 +2,7 @@
 // Copyright (c) 2009-2015 The Bitcoin developers
 // Copyright (c) 2009-2015 The Dash developers
 // Copyright (c) 2015-2017 The AmsterdamCoin developers
-// Copyright (c) 2017 The PUFFScoin developers
+// Copyright (c) 2017 The PuffsCoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -68,10 +68,10 @@ static bool AppInitRPC(int argc, char* argv[])
     //
     ParseParameters(argc, argv);
     if (argc < 2 || mapArgs.count("-?") || mapArgs.count("-help") || mapArgs.count("-version")) {
-        std::string strUsage = _("PUFFSCoin Core RPC client version") + " " + FormatFullVersion() + "\n";
+        std::string strUsage = _("PuffsCoin Core RPC client version") + " " + FormatFullVersion() + "\n";
         if (!mapArgs.count("-version")) {
             strUsage += "\n" + _("Usage:") + "\n" +
-                        "  puffscoin-cli [options] <command> [params]  " + _("Send command to PUFFSCoin Core") + "\n" +
+                        "  puffscoin-cli [options] <command> [params]  " + _("Send command to PuffsCoin Core") + "\n" +
                         "  puffscoin-cli [options] help                " + _("List commands") + "\n" +
                         "  puffscoin-cli [options] help <command>      " + _("Get help for a command") + "\n";
 
