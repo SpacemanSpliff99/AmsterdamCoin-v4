894a8ebba5f7053ffebb59028a1ca9f2db1fc5e2
SpacemanSpliff99 <33433017+SpacemanSpliff99@users.noreply.github.com>
2017-11-24 21:33:10 -0400

    Update obfuscation.h

--- src/obfuscation.h
+++ src/obfuscation.h
@@ -1,5 +1,6 @@
 // Copyright (c) 2014-2015 The Dash developers
 // Copyright (c) 2015-2017 The AmsterdamCoin developers
+// Copyright (c) 2017 The PuffsCoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -493,7 +494,7 @@ public:
 
     void GetDenominationsToString(int nDenom, std::string& strDenom);
 
-    /// Get the denominations for a specific amount of amsterdamcoin.
+    /// Get the denominations for a specific amount of puffscoin.
     int GetDenominationsByAmount(int64_t nAmount, int nDenomTarget = 0); // is not used anymore?
     int GetDenominationsByAmounts(std::vector<int64_t>& vecAmount);
 
