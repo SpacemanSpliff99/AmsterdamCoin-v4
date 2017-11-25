2e960cea3ba40a87e840e2ae894b0f3e0eb70a59
SpacemanSpliff99 <33433017+SpacemanSpliff99@users.noreply.github.com>
2017-11-24 22:01:23 -0400

    Update util.h

--- src/util.h
+++ src/util.h
@@ -2,6 +2,7 @@
 // Copyright (c) 2009-2014 The Bitcoin developers
 // Copyright (c) 2014-2015 The Dash developers
 // Copyright (c) 2015-2017 The AmsterdamCoin developers
+// Copyright (c) 2017 The PuffsCoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -13,7 +14,7 @@
 #define BITCOIN_UTIL_H
 
 #if defined(HAVE_CONFIG_H)
-#include "config/amsterdamcoin-config.h"
+#include "config/puffscoin-config.h"
 #endif
 
 #include "compat.h"
@@ -29,14 +30,14 @@
 #include <boost/filesystem/path.hpp>
 #include <boost/thread/exceptions.hpp>
 
-//AmsterdamCoin only features
+//PuffsCoin only features
 
 extern bool fMasterNode;
 extern bool fLiteMode;
 extern bool fEnableSwiftTX;
 extern int nSwiftTXDepth;
 extern int nObfuscationRounds;
-extern int nAnonymizeAmsterdamCoinAmount;
+extern int nAnonymizePuffsCoinAmount;
 extern int nLiquidityProvider;
 extern bool fEnableObfuscation;
 extern int64_t enforceMasternodePaymentsTime;
@@ -213,7 +214,7 @@ void RenameThread(const char* name);
 template <typename Callable>
 void LoopForever(const char* name, Callable func, int64_t msecs)
 {
-    std::string s = strprintf("amsterdamcoin-%s", name);
+    std::string s = strprintf("puffscoin-%s", name);
     RenameThread(s.c_str());
     LogPrintf("%s thread start\n", name);
     try {
@@ -239,7 +240,7 @@ void LoopForever(const char* name, Callable func, int64_t msecs)
 template <typename Callable>
 void TraceThread(const char* name, Callable func)
 {
-    std::string s = strprintf("amsterdamcoin-%s", name);
+    std::string s = strprintf("puffscoin-%s", name);
     RenameThread(s.c_str());
     try {
         LogPrintf("%s thread start\n", name);
