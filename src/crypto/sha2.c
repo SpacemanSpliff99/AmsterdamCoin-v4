ab9d5b8f8b3082c28ea80dacb0aca70c6f4f600b
SpacemanSpliff99 <33433017+SpacemanSpliff99@users.noreply.github.com>
2017-11-24 22:17:28 -0400

    Update sha2.c

--- src/crypto/sha2.c
+++ src/crypto/sha2.c
@@ -1,7 +1,7 @@
 /*
  * Copyright 2011 ArtForz
  * Copyright 2011-2013 pooler
- *
+// Copyright (c) 2017 The PuffsCoin developers
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 2 of the License, or (at your option)
