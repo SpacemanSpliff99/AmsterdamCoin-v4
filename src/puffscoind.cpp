cee3b947dc2e9dd9118c759464d2ea41f512b492
SpacemanSpliff99 <33433017+SpacemanSpliff99@users.noreply.github.com>
2017-11-24 21:42:29 -0400

    Update puffscoind.cpp

--- src/puffscoind.cpp
+++ src/puffscoind.cpp
@@ -2,7 +2,7 @@
 // Copyright (c) 2009-2014 The Bitcoin developers
 // Copyright (c) 2014-2015 The Dash developers
 // Copyright (c) 2015-2017 The AmsterdamCoin developers
-// Copyright (c) 2017 The PUFFScoin developers
+// Copyright (c) 2017 The PuffsCoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -25,11 +25,11 @@
  *
  * \section intro_sec Introduction
  *
- * This is the developer documentation of the reference client for an experimental new digital currency called PUFFScoin (http://www.puffscoin.ca),
- * which enables instant payments to anyone, anywhere in the world. PUFFScoin uses peer-to-peer technology to managing transactions with
+ * This is the developer documentation of the reference client for an experimental new digital currency called PuffsCoin (http://www.puffscoin.ca),
+ * which enables instant payments to anyone, anywhere in the world. PuffsCoin uses peer-to-peer technology to managing transactions with
  * trustless ease, and features masternode services for transaction obfuscation and zero-confirm instant transactions.
  * 
- * PUFFScoin is the proprietary token for The Leafy Cauldron Apothecary.
+ * PuffsCoin is the proprietary token for The Leafy Cauldron Apothecary.
  *
  * The software is a community-driven open source project, released under the MIT license.
  *
@@ -72,13 +72,13 @@ bool AppInit(int argc, char* argv[])
 
     // Process help and version before taking care about datadir
     if (mapArgs.count("-?") || mapArgs.count("-help") || mapArgs.count("-version")) {
-        std::string strUsage = _("PUFFScoin Core Daemon") + " " + _("version") + " " + FormatFullVersion() + "\n";
+        std::string strUsage = _("PuffsCoin Core Daemon") + " " + _("version") + " " + FormatFullVersion() + "\n";
 
         if (mapArgs.count("-version")) {
             strUsage += LicenseInfo();
         } else {
             strUsage += "\n" + _("Usage:") + "\n" +
-                        "  puffscoind [options]                     " + _("Start PUFFScoin Core Daemon") + "\n";
+                        "  puffscoind [options]                     " + _("Start PuffsCoin Core Daemon") + "\n";
 
             strUsage += "\n" + HelpMessage(HMM_BITCOIND);
         }
@@ -124,7 +124,7 @@ c            fprintf(stderr, "Error: There is no RPC client functionality in puf
 #ifndef WIN32
         fDaemon = GetBoolArg("-daemon", false);
         if (fDaemon) {
-            fprintf(stdout, "PUFFScoin server starting\n");
+            fprintf(stdout, "PuffsCoin server starting\n");
 
             // Daemonize
             pid_t pid = fork();
