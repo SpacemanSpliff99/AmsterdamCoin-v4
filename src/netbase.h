8135816376b7b74f5ba3f31118c2b77fa2dace2c
SpacemanSpliff99 <33433017+SpacemanSpliff99@users.noreply.github.com>
2017-11-24 21:31:16 -0400

    Update netbase.h

--- src/netbase.h
+++ src/netbase.h
@@ -1,4 +1,5 @@
 // Copyright (c) 2009-2013 The Bitcoin developers
+// Copyright (c) 2017 The PuffsCoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -6,7 +7,7 @@
 #define BITCOIN_NETBASE_H
 
 #if defined(HAVE_CONFIG_H)
-#include "config/amsterdamcoin-config.h"
+#include "config/puffscoin-config.h"
 #endif
 
 #include "compat.h"
