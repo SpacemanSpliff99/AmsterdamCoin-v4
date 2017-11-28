589dc3628a95fb51832fda0eb25d371ec704943b
SpacemanSpliff99 <33433017+SpacemanSpliff99@users.noreply.github.com>
2017-11-28 17:40:28 -0400

    Update linux.mk

--- depends/builders/linux.mk
+++ depends/builders/linux.mk
@@ -1,3 +1,2 @@
-// Copyright (c) 2017 The PuffsCoin developers
 build_linux_SHA256SUM = sha256sum
 build_linux_DOWNLOAD = curl --location --fail --connect-timeout $(DOWNLOAD_CONNECT_TIMEOUT) --retry $(DOWNLOAD_RETRIES) -o
