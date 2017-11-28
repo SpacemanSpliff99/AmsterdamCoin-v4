e9275b99c99acab9d6fe3d1b02300106a7321d5a
SpacemanSpliff99 <33433017+SpacemanSpliff99@users.noreply.github.com>
2017-11-27 21:03:43 -0400

    Update bitcoinaddressvalidator.cpp

--- src/qt/bitcoinaddressvalidator.cpp
+++ src/qt/bitcoinaddressvalidator.cpp
@@ -82,7 +82,7 @@ BitcoinAddressCheckValidator::BitcoinAddressCheckValidator(QObject* parent) : QV
 QValidator::State BitcoinAddressCheckValidator::validate(QString& input, int& pos) const
 {
     Q_UNUSED(pos);
-    // Validate the passed AmsterdamCoin address
+    // Validate the passed PUFFScoin address
     CBitcoinAddress addr(input.toStdString());
     if (addr.IsValid())
         return QValidator::Acceptable;
