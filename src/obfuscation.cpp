34218dd0de45a79f5b7eec2b869222bb5692bade
SpacemanSpliff99 <33433017+SpacemanSpliff99@users.noreply.github.com>
2017-12-01 17:33:41 -0400

    Update obfuscation.cpp

--- src/obfuscation.cpp
+++ src/obfuscation.cpp
@@ -780,7 +780,7 @@ void CObfuscationPool::ChargeRandomFees()
                 with using it to stop abuse. Otherwise it could serve as an attack vector and
                 allow endless transaction that would bloat PuffsCoin and make it unusable. To
                 stop these kinds of attacks 1 in 10 successful transactions are charged. This
-                adds up to a cost of 0.001 AMS per transaction on average.
+                adds up to a cost of 0.001 PUFFS per transaction on average.
             */
             if (r <= 10) {
                 LogPrintf("CObfuscationPool::ChargeRandomFees -- charging random fees. %u\n", i);
